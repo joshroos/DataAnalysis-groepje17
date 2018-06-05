e735ff79841303d7480230bf5d09fe04a4cebe18
EllenBogaards <ellenbogaards@gmail.com>
2018-06-05 13:45:52 +0200

    testcsv2
    testcsv2

--- code/data_clean.py
+++ code/data_clean.py
@@ -4,4 +4,7 @@ import numpy as np
 rng = np.random.RandomState(42)
 df = pd.DataFrame(rng.randint(0, 10, (10,5)),
                   columns=['A', 'B', 'C', 'D', 'E'])
-print(df)
+#print(df)
+
+data = pd.read_csv('testWFP.csv')
+print(data)

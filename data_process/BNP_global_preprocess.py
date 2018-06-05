e092ecb30da891b879657c5530ad7c70d35f215d
Darius Barsony <dariusbarsony@gmail.com>
2018-06-05 14:22:41 +0200

    Processen BNP data

--- data_process/BNP_global_preprocess.py
+++ data_process/BNP_global_preprocess.py
@@ -1,4 +1,6 @@
 import pandas as pd
 import  numpy as np 
 
-pd.read_csv("/home/darius/Documents/DataAnalysis-groepje17/data/BNP data/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_9944656.csv")
+dataframe = pd.read_csv("/home/darius/Documents/DataAnalysis-groepje17/data/BNP data/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_9944656.csv", header = 2)
+df1 = dataframe["Country Name"]
+print(df1)

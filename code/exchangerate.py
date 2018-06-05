f50ed55c7326d8640f0c49f00b97b48afcdd5c63
rjvervelde <33282439+rjvervelde@users.noreply.github.com>
2018-06-05 14:50:02 +0200

    works
    works

--- code/exchangerate.py
+++ code/exchangerate.py
@@ -4,14 +4,7 @@ print("Helloo")
 
 import matplotlib as plt
 import pandas as pd
-import os 
-os.getcwd()
-# your current working directory will be displayed
-print(os.path.exists('exchangerate.csv'))
-# Must be True, otherwise it is unrelated to pandas
-dir_path = os.path.dirname(os.path.realpath('exchangerate.csv'))
-print(dir_path)
 
-data = pd.read_csv('Users\Gebruiker\Documents\GitHub\DataAnalysis-groepje17\code\exchangerate.csv', encoding='latin-1')
+data = pd.read_csv('..\code\exchangerate.csv', encoding='latin-1')
 
 print(data)

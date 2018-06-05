7ff5a5a458cb66ca3cbb6df45814c02fed755362
Joshua de Roos <joshua_deroos@hotmail.com>
2018-06-05 14:19:07 +0200

    test excel bestand

--- code/data_clean.py
+++ code/data_clean.py
@@ -5,8 +5,9 @@
 import matplotlib as plt
 import pandas as pd
 
-
-
+# Transformeert het csv bestand naar panda dataframe
 data = pd.read_csv('../data/WFPVAM_FoodPrices_05-12-2017.csv', encoding='latin-1')
 
-print(data)
+writer = pd.ExcelWriter('output.xlsx')
+data.to_excel(writer)
+writer.save()

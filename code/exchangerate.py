619ee5864ccf8ca8754b38763fa3364ca6763e8e
rjvervelde <33282439+rjvervelde@users.noreply.github.com>
2018-06-05 14:24:17 +0200

    x
    x

--- code/exchangerate.py
+++ code/exchangerate.py
@@ -2,4 +2,11 @@
 
 print("Helloo")
 
+import matplotlib as plt
+import pandas as pd
 
+
+
+data = pd.read_csv('...\Gebruiker\Documents\GitHub\DataAnalysis-groepje17\data\exchangerate.csv', encoding='latin-1')
+
+print(data)

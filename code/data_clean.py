7435744960fb8fef125b70342074139ad5c9792a
EllenBogaards <ellenbogaards@gmail.com>
2018-06-07 11:36:25 +0200

    population
    werken aan pop grow

--- code/data_clean.py
+++ code/data_clean.py
@@ -1,25 +1,18 @@
-<<<<<<< HEAD
 import pandas as pd
 import numpy as np
+import matplotlib as plt
 
 rng = np.random.RandomState(42)
 df = pd.DataFrame(rng.randint(0, 10, (10,5)),
                   columns=['A', 'B', 'C', 'D', 'E'])
 #print(df)
 
-data = pd.read_csv('testWFP.csv')
-print(data)
-=======
-# Joshua de Roos en Ellen Bogaards
-# 5 juni 2018
-# Dit programma maakt van het csv-bestand van de WFP een overzichtelijke tabel
+data = pd.read_csv('../data/WFPVAM_FoodPrices_05-12-2017.csv',encoding='latin-1')
+#print(data.head())
+#print(data)
+#print(data['adm0_name'])
+#print(data[['adm0_id','adm0_name']])
+#print(data['adm0_name'].unique())
 
-import matplotlib as plt
-import pandas as pd
-
-
-
-data = pd.read_csv('../data/WFPVAM_FoodPrices_05-12-2017.csv', encoding='latin-1')
-
-print(data)
->>>>>>> master
+data_population = pd.read_csv('../data/population_countries_1960-2016.csv', encoding='latin-1')
+print(data[1:20])
---
+++ code/data_clean_population.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+data_population = pd.read_csv('../data/population_countries_1960-2016.csv', encoding='latin-1')
+print(data_population)

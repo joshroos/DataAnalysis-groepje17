0de302bba73a9f5aea28c1892a366f9ecf1ff684
Darius Barsony <dariusbarsony@gmail.com>
2018-06-07 13:24:49 +0200

    aanpassingen in global preprocess

--- code/data_clean.py
+++ code/data_clean.py
@@ -5,8 +5,11 @@
 import matplotlib as plt
 import pandas as pd
 
-
-
 data = pd.read_csv('../data/WFPVAM_FoodPrices_05-12-2017.csv', encoding='latin-1')
 
-print(data)
+# hier lees ik alle landen in en schrijf deze naar een ander bestand
+countries = data['adm0_name'].unique()
+all_countries = open('../data/BNP data/countries.csv', 'w')
+for country in countries:
+    all_countries.write("%s," % country)
+
--- data_process/BNP_global_preprocess.py
+++ data_process/BNP_global_preprocess.py
@@ -1,9 +1,20 @@
 import pandas as pd
 import  numpy as np 
 
-dataframe = pd.read_csv("/home/darius/Documents/DataAnalysis-groepje17/data/BNP data/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_9944656.csv", header = 2)
+dataframe = pd.read_csv("../data/BNP data/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_9944656.csv", header = 2)
+countries_file = pd.read_csv("../data/BNP data/countries.csv")
+
+countries = []
+for country in countries_file:
+    # if country != 'Unnamed: 74':
+    countries.append(country)
 
 # drop columns that are unnecessary for analysis
 processed_data = dataframe.drop(columns = list(dataframe)[4:36])
 
-print(processed_data)
+countries_BNP = processed_data["Country Name"]
+common_countries = set(countries_BNP).intersection(countries)
+
+# main_list = np.setdiff1d(common_countries, set(countries))
+
+print(len(countries), countries_BNP)

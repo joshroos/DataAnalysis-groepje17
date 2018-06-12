d2f0862c32b6065154724be5d205e84fc4967b1e
EllenBogaards <ellenbogaards@gmail.com>
2018-06-12 12:02:34 +0200

    rainfall
    alle eerste twaalf regels van pr (regenval) opsommen, zodat het per jaar wordt ipv per maand

--- code/rainfall.py
+++ code/rainfall.py
@@ -3,5 +3,16 @@ import pandas as pd
 import io
 from pandas import ExcelWriter
 
-rainfall_1991_2015 = pd.read_csv('../data/EXCEL_RAINFALL_COUNTRIES.csv',header=0, sep=',', error_bad_lines=False, encoding = 'latin-1')
-print(rainfall_1991_2015)
+rainfall = pd.read_csv('../data/EXCEL_RAINFALL_COUNTRIES.csv',header=0, sep=',', error_bad_lines=False, encoding = 'latin-1')
+#print(rainfall_1991_2015)
+counter = 0
+rainfall['per_year'] = 0
+for i in range(21301):
+    if counter < 12:
+        rainfall['per_year'][i] += rainfall['pr'][i]
+        counter += 1
+        print(rainfall['per_year'])
+    else:
+        print("hoi")
+
+#elke 12 waardes moeten samengevoegd worden maar lukt niet

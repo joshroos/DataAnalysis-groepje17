04b8a62732e5ccd5ad4f0c952fae9cf22229c4f8
rjvervelde <33282439+rjvervelde@users.noreply.github.com>
2018-06-12 15:07:18 +0200

    x

--- visualizations/EDA.py
+++ visualizations/EDA.py
@@ -1,6 +1,24 @@
 from bokeh.plotting import figure
 from bokeh.io import output_file, show
-import pandas
+import matplotlib as plt
+import pandas as pd
+import io
+from pandas import ExcelWriter
 
-rainfall = pd.read_csv('../data/rainfall_better.csv',header=0, sep=',', error_bad_lines=False, encoding = 'latin-1')
-exchangerate = pd.read_excel('../data/exchangerate_simple.xlsx',header=0, sep=',', error_bad_lines=False, encoding = 'latin-1')
+rainfall = pd.read_csv('../code/rainfall_better.csv',header=0, sep=',', error_bad_lines=False, encoding = 'latin-1')
+exchangerate = pd.read_excel('../code/exchangerate_simple.xlsx',header=0, sep=',', error_bad_lines=False, encoding = 'latin-1')
+
+# Prepare some data
+x = rainfall["year"]
+y = rainfall["pr_total"]
+
+# Prepare the output file
+output_file("rainfall.html")
+
+# Create a figure object
+f = figure()
+
+# Create line plot
+f.line(x,y)
+
+show(f)

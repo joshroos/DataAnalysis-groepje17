fa546a6cf0957d826b2cfed46f86c30cf4377729
rjvervelde <33282439+rjvervelde@users.noreply.github.com>
2018-06-12 14:20:31 +0200

    rainfall made
    rainfall made

--- code/rainfall.py
+++ code/rainfall.py
@@ -2,17 +2,25 @@ import matplotlib as plt
 import pandas as pd
 import io
 from pandas import ExcelWriter
+import WFP_clean
 
 rainfall = pd.read_csv('../data/EXCEL_RAINFALL_COUNTRIES.csv',header=0, sep=',', error_bad_lines=False, encoding = 'latin-1')
-#print(rainfall_1991_2015)
-counter = 0
-rainfall['per_year'] = 0
-for i in range(21301):
-    if counter < 12:
-        rainfall['per_year'][i] += rainfall['pr'][i]
-        counter += 1
-        print(rainfall['per_year'])
-    else:
-        print("hoi")
+data_WFP = pd.read_csv('../data/WFPVAM_FoodPrices_05-12-2017.csv', encoding='latin-1')
+countrycodes = pd.read_csv('../data/countrycodes.csv', header = 0, sep = ',', error_bad_lines=False, encoding = 'latin-1')
+data_WFP = WFP_clean.data_clean(data_WFP)
 
-#elke 12 waardes moeten samengevoegd worden maar lukt niet
+download = "rainfall_better.csv" 
+csv = open(download, "w") 
+columnTitleRow = "country, year, pr_total\n"
+csv.write(columnTitleRow)
+
+countries = rainfall['Country_Code'].unique()
+
+for country in countries:
+    for year in range(1991, 2015):
+        pr_new = rainfall.loc[(rainfall['Year'] == year) &  (rainfall['Country_Code'] == country), 'pr']
+        pr_total = pr_new.sum()
+        country_name = list(countrycodes.loc[countrycodes['alpha-3'] == country, 'name'])
+        row = str(country_name[0]) + "," + str(year) + "," + str(pr_total) + "\n"
+        csv.write(row)
+

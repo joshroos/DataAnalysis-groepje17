3d13c52359a01b5c44121a8840c270a84e73a4f6
rjvervelde <33282439+rjvervelde@users.noreply.github.com>
2018-06-13 12:54:21 +0200

    visualization
    visualization

--- visualizations/EDA.py
+++ visualizations/EDA.py
@@ -1,5 +1,6 @@
 from bokeh.plotting import figure
 from bokeh.io import output_file, show
+from bokeh.models import LinearAxis, Range1d
 import matplotlib as plt
 import pandas as pd
 import io
@@ -9,42 +10,67 @@ rainfall = pd.read_csv('../data/rainfall_better.csv',header=0, sep=',', error_ba
 exchangerate = pd.read_excel('../data/exchangerate_simple.xlsx',header=0, sep=',', error_bad_lines=False, encoding = 'latin-1')
 data_WFP = pd.read_csv('../data/WFP_data_normalised.csv', encoding='latin-1')
 
-years = [x for x in range(1992, 2015)]
-print(data_WFP.loc[data_WFP["cm_name"] == "Wheat"])
+years = [x for x in range(1992, 2014)]
+
+#########################
+prices = []
+
+for year in years:
+
+    month = data_WFP['mp_month'] == 6
+    year = data_WFP['mp_year'] == year
+    country = data_WFP['adm0_name'] == 'Afghanistan'
+    cm_name = data_WFP['cm_name'] == 'Wheat'
+
+    val = data_WFP.loc[(month) & (year) & (country) & (cm_name), 'mp_price']
+    prices.append(val.mean())
+
+#output_file("wheat_new.html")
+#f = figure()
+#f.line(years,prices)
+#show(f)
+
+#########################
+years = [x for x in range(1992, 2014)]
 
 
+rainfall_afghanistan = []
 
-# Prepare some data
-print(data_WFP.loc[(data_WFP["cm_name"] == "Wheat") & (data_WFP["adm0_name"] == "Afghanistan"), 'mp_price'].mean())
+for year in years:
+    year_info = rainfall['year'] == year
 
-wheat = data_WFP.loc[(data_WFP["cm_name"] == "Wheat") & (data_WFP["adm0_name"] == "Afghanistan"), 'mp_price']
-years_w = data_WFP.loc[(data_WFP["cm_name"] == "Wheat"), 'mp_year']
+    country = rainfall['country'] == 'Afghanistan'
+    
+    val = rainfall.loc[(year_info) & (country), 'pr_total']
+    rainfall_afghanistan.append(val)
 
-# Prepare the output file
-output_file("wheat.html")
 
-# Create a figure object
-g = figure()
+#output_file("rainfall.html")
+#h = figure()
+#h.line(years, rainfall_afghanistan)
+#show(h)
 
-# Create line plot
-g.line(years_w, wheat)
+#########################
 
-show(g)
+# Seting the params for the first figure.
+s1 = figure(x_axis_type="datetime", plot_width=1000, plot_height=600)
 
-rainfall = rainfall.loc[rainfall['pr_total']]
-years_r = rainfall.loc[rainfall['years']]
+# Setting the second y axis range name and range
+s1.extra_y_ranges = {"foo": Range1d(start=0, end=500)}
 
-# Prepare the output file
-output_file("rainfall.html")
+# Adding the second axis to the plot.  
+s1.add_layout(LinearAxis(y_range_name="foo"), 'right')
 
-# Create a figure object
-h = figure()
+# Setting the rect glyph params for the first graph. 
+# Using the default y range and y axis here.           
+s1.line(years, prices)
 
-# Create line plot
-h.line(years_r, rainfall)
+# Setting the rect glyph params for the second graph. 
+# Using the aditional y range named "foo" and "right" y axis here. 
+s1.line(years, rainfall_afghanistan, y_range_name="foo")
 
-show(h)
-
+# Show the combined graphs with twin y axes.
+show(s1)
 
 '''
 prices = []

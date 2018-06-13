8d446030746044b34409db1d9313b7834f362dec
Joshua de Roos <joshua_deroos@hotmail.com>
2018-06-13 12:41:48 +0200

    Rainfall per maand

--- visualizations/eda_josh.py
+++ visualizations/eda_josh.py
@@ -20,7 +20,7 @@ goed2 = data_WFP.loc[data_WFP['cm_name'].str.contains('Cheese'), 'adm0_name'].un
 goed = list(set(goed1) & set(goed2))
 goed.sort()
 
-def correlations(data_WFP):
+def correlations_per_country(data_WFP):
     years = [x for x in range(1992, 2018)]
     countries = data_WFP['adm0_name'].unique()
 
@@ -43,8 +43,10 @@ def correlations(data_WFP):
                             time = data_WFP['mp_year'] == year
                             for month in range(1, 13):
                                 data_month = data_WFP['mp_month'] == month
-                                list_good1 = data_WFP.loc[country & good1 & time & data_month, 'mp_price']
-                                list_good2 = data_WFP.loc[country & good2 & time & data_month, 'mp_price']
+                                list_good1 = data_WFP.loc[country & good1 &
+                                 time & data_month, 'mp_price']
+                                list_good2 = data_WFP.loc[country & good2 & 
+                                time & data_month, 'mp_price']
 
                                 if list_good2.empty is False and list_good1.empty is False:
                                     prices1.append(list_good1.mean())
@@ -53,8 +55,45 @@ def correlations(data_WFP):
                         if prices1 and prices2 and len(prices1) > 10:
                             coeff = np.corrcoef(prices1, prices2)
                             if coeff[0][1] > 0.90 or coeff[0][1] < -0.90:
-                                print("{}:  {} and  {}  =   {}      {}".format(land, i, j, round(coeff[0][1], 4), len(prices1)))
-correlations(data_WFP)
+                                print("{}:  {} and  {}  =   {}      {}".format
+                                (land, i, j, round(coeff[0][1], 4), len(prices1)))
+#correlations(data_WFP)
+
+def correlations_per_good(data_WFP):
+    done = []
+    goods = data_WFP['cm_name'].unique()
+    years = [x for x in range(1992, 2018)]
+    for i in goods:
+        for j in goods:
+            if i != j:
+                if (i,j) not in done and (j,i) not in done:
+                    done.append((i,j))
+                    good1 = data_WFP['cm_name'] == i
+                    good2 = data_WFP['cm_name'] == j
+
+                    prices1 = []
+                    prices2 = []
+
+                    for year in years:
+                        time = data_WFP['mp_year'] == year
+                        for month in range(1, 13):
+                            data_month = data_WFP['mp_month'] == month
+                            list_good1 = data_WFP.loc[good1 &
+                                time & data_month, 'mp_price']
+                            list_good2 = data_WFP.loc[good2 & 
+                            time & data_month, 'mp_price']
+
+                            if list_good2.empty is False and list_good1.empty is False:
+                                prices1.append(list_good1.mean())
+                                prices2.append(list_good2.mean())
+
+                    if prices1 and prices2 and len(prices1) > 10:
+                        coeff = np.corrcoef(prices1, prices2)
+                        if coeff[0][1] > 0.90 or coeff[0][1] < -0.90:
+                            print("{} and  {}  =   {}      {}".format
+                            (i, j, round(coeff[0][1], 4), len(prices1)))
+
+correlations_per_good(data_WFP)
 
 # units = data_WFP.loc[data_WFP['cm_name'] == 'Flour', 'um_name'].unique()
 # units.sort()

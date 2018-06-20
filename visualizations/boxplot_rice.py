46ec9b350647b84b375a049848ced948b8bc91cd
Joshua de Roos <joshua_deroos@hotmail.com>
2018-06-20 14:36:01 +0200

    Boxplot

--- visualizations/boxplot_rice.py
+++ visualizations/boxplot_rice.py
@@ -50,7 +50,7 @@ qmin = groups.quantile(q=0.00)
 qmax = groups.quantile(q=1.00)
 upper.score = [min([x,y]) for (x,y) in zip(list(qmax.loc[:,'score']),upper.score)]
 lower.score = [max([x,y]) for (x,y) in zip(list(qmin.loc[:,'score']),lower.score)]
-
+print(lower.score)
 # stems
 p.segment(countries, upper.score, countries, q3.score, line_color="black")
 p.segment(countries, lower.score, countries, q1.score, line_color="black")
@@ -60,8 +60,8 @@ p.vbar(countries, 0.7, q2.score, q3.score, fill_color="#9B59B6", line_color="bla
 p.vbar(countries, 0.7, q1.score, q2.score, fill_color="#C0392B", line_color="black")
 
 # whiskers (almost-0 height rects simpler than segments)
-p.rect(countries, lower.score, 0.2, 0.01, line_color="black")
 p.rect(countries, upper.score, 0.2, 0.01, line_color="black")
+p.rect(countries, lower.score, 0.2, 0.01, line_color="black")
 
 p.xgrid.grid_line_color = None
 p.ygrid.grid_line_color = "white"

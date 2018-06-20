164f1bc3a8b6407d81fb93a3e169ec3f095e42f5
rjvervelde <33282439+rjvervelde@users.noreply.github.com>
2018-06-20 16:04:00 +0200

    correlations geanalyzeerd
    correlations geanalyzeerd

--- visualizations/correlation_goods.py
+++ visualizations/correlation_goods.py
@@ -12,18 +12,86 @@ data_WFP = pd.read_csv('../data/WFP_data_normalised.csv', encoding='latin-1')
 data_correlation = pd.read_csv('../visualizations/corrcoef.csv', encoding='latin-1')
 
 def best_correlation(data_correlation):
-    correlations = data_correlation.loc[(data_correlation['correlation'] >= 0.9), ['product1', 'product2', 'Country']]
-    print(correlations)
-
+    
     products = data_WFP['cm_name'].unique()
+    #products = ["Wheat", "Millet", "Maize"]
+    countries = []
+    all_amounts = []
+    combinations = []
+    correlations = []
 
     for product1 in products:
         for product2 in products:
+            if product1 != product2:
+                group_correlations = []
+                group_countries = []
+                group_combinations = []
+                amount = 0
+                correlation1 = data_correlation.loc[(data_correlation['product1'] == product1) & (data_correlation['product2'] == product2),'correlation']
+                country1 = data_correlation.loc[(data_correlation['product1'] == product1) & (data_correlation['product2'] == product2),'Country']
+                correlation2 = data_correlation.loc[(data_correlation['product1'] == product2) & (data_correlation['product2'] == product1),'correlation'] 
+                country2 = data_correlation.loc[(data_correlation['product1'] == product2) & (data_correlation['product2'] == product1),'Country']
+                found_correlations = correlation1.tolist() + correlation2.tolist()
+                #print("correlation", found_correlations)
+                
+                found_countries = country1.tolist() + country2.tolist()
+                #print("countries", found_countries)
+                n = 0
 
-            countries = correlations.loc[(correlation['product1'] == product1 or product2) and (correlation['product2'] == product1 or product2), 'Country']
-    
-    
-    return
+                for correlation_found in found_correlations:
+                    
+                    if correlation_found >= 0.75:
+                        group_correlations.append(correlation_found)
+                        group_countries.append(found_countries[n])
+                        amount += 1
+                        combi = []
+                        combi.append(product1)
+                        combi.append(product2)
+                        group_combinations.append(combi)
+                        n += 1
+                
+                if group_countries != []:
+                    countries.append(group_countries)
+                
+                if group_correlations != []:
+                    correlations.append(group_correlations)
 
-best_correlation(data_correlation)
+                if group_combinations != []:
+                    combinations.append(group_combinations[0])
+                
+                if n != 0:
+                    all_amounts.append(amount)
+                
+    print(correlations, countries, all_amounts, combinations)
+    print(len(correlations), len(countries), len(all_amounts), len(combinations))
+
+    download = "combination_correlations.csv" 
+    csv = open(download, "w") 
+    columnTitleRow = "correlation, country, n, combinations\n"
+    csv.write(columnTitleRow)
+
+    for i in range(len(correlations)):
+        row = str(correlations[i])  + "," + str(countries[i]) + "," + str(all_amounts[i]) + "," + str(combinations[i]) + "\n"
+        csv.write(row)
+
+    return         
+
+data_combinations = pd.read_csv('../visualizations/combination_correlations.csv', encoding='latin-1')
+
+def find_things(data_combinations):
+
+    correlations_combinations = data_combinations.loc[data_combinations['n'] >= 10, 'combinations']
+    correlations_countries = data_combinations.loc[data_combinations['n'] >= 10, 'country']
+    correlations = data_combinations.loc[data_combinations['n'] >= 10, 'correlation']
+    combinations = correlations_combinations.tolist()
+    countries = correlations_countries.tolist()
+    correlations = correlations.tolist()
+    print(countries)
+    print(combinations)
+    print(correlations)
+    return 
+
+print(find_things(data_combinations))
+
+#best_correlation(data_correlation)
 
--- visualizations/rainfall_correlation.py
+++ visualizations/rainfall_correlation.py
@@ -28,6 +28,7 @@ def rainfall_correlation(rainfall, data_WFP):
 
         val = data_WFP.loc[(data_WFP['cm_name'] == k), 'adm0_name']
         countries = val.unique()
+        country_withcorrelation = []
 
         for j in countries:
             n = 0
@@ -48,24 +49,25 @@ def rainfall_correlation(rainfall, data_WFP):
                 else:
                     prices.append(val1.mean())
                     rainfall_country.append(val2.mean())
-                    all_products.append(k)
                     n += 1
             
             correlation = np.corrcoef(prices, rainfall_country)
+            all_products.append(k)
+            country_withcorrelation.append(j)
             print(j, k, correlation)
             correlations.append(correlation[0,1])
             Amount_of_n.append(n)
      
 
-    print(countries, Amount_of_n, correlations)
+    print(country_withcorrelation, Amount_of_n, all_products, correlations)
 
-    download = "rainfall_wheat_correlations.csv" 
+    download = "rainfall_correlations.csv" 
     csv = open(download, "w") 
     columnTitleRow = "product, country, n, correlation\n"
     csv.write(columnTitleRow)
 
-    for i in range(len(countries)):
-        row = str(all_products[i])  + "," + str(countries[i]) + "," + str(Amount_of_n[i]) + "," + str(correlations[i]) + "\n"
+    for i in range(len(country_withcorrelation)):
+        row = str(all_products[i])  + "," + str(country_withcorrelation[i]) + "," + str(Amount_of_n[i]) + "," + str(correlations[i]) + "\n"
         csv.write(row)
 
     return

4b580b507db2fe2120b8a8f839f53eb08a647d0c
rjvervelde <33282439+rjvervelde@users.noreply.github.com>
2018-06-28 15:13:43 +0200

    opgeschoond

--- code/week1_preprocess/rainfall.py
+++ code/week1_preprocess/rainfall.py
@@ -9,22 +9,24 @@ data_WFP = pd.read_csv('../../data/WFPVAM_FoodPrices_05-12-2017.csv', encoding='
 countrycodes = pd.read_csv('../../data/countrycodes.csv', header = 0, sep = ',', error_bad_lines=False, encoding = 'latin-1')
 data_WFP = wfp_clean.data_clean(data_WFP)
 
+# 
 def rainfall_per_year():
     download = "rainfall_better.csv" 
     csv = open(download, "w") 
     columnTitleRow = "country, year, pr_total\n"
     csv.write(columnTitleRow)
 
-    countries = rainfall['Country_Code'].unique()
+    countries = rainfall['Country'].unique()
 
     for country in countries:
         for year in range(1991, 2015):
-            pr_new = rainfall.loc[(rainfall['Year'] == year) &  (rainfall['Country_Code'] == country), 'pr']
+            pr_new = rainfall.loc[(rainfall['Year'] == year) &  (rainfall['Country'] == country), 'pr']
             pr_total = pr_new.sum()
             country_name = list(countrycodes.loc[countrycodes['alpha-3'] == country, 'name'])
             if country == 'ISR':
                 print("Here is Israel")
                 country_name = ['State of Palestine']
+
             row = str(country_name[0]) + "," + str(year) + "," + str(pr_total) + "\n"
             csv.write(row)
 
@@ -32,7 +34,7 @@ rainfall_per_year()
 
 # changes the country codes in de rainfall file to country name
 def change_country_code(rainfall):
-    countries = rainfall['Country_Code'].unique()
+    countries = rainfall['Country'].unique()
     
     for code in countries:
         country = list(countrycodes.loc[countrycodes['alpha-3'] == code, 'name'])
@@ -40,8 +42,7 @@ def change_country_code(rainfall):
         if code == 'ISR':
             print("Here is Israel")
             country = ['State of Palestine']
-        rainfall.loc[rainfall['Country_Code'] == code, 'Country_Code'] = country[0]
+        rainfall.loc[rainfall['Country'] == code, 'Country'] = country[0]
     
     rainfall.to_csv('rainfall_per_month.csv')
-#change_country_code(rainfall)
 
